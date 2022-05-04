484007ff886170b416f184090a9faed9d43e4499
Ovidijus <ovidijus.e@gmail.com>
2022-05-04 13:53:05 +0300

    get route

--- src/routes/vetRoutes.js
+++ src/routes/vetRoutes.js
@@ -14,4 +14,21 @@ vetRoutes.post('/pets/create', controller.createTable);
 
 vetRoutes.post('/pets', controller.createCategory);
 
+vetRoutes.get('/pets', async (req, res) => {
+  let conn;
+  try {
+    // 1 prisijungti
+    conn = await mysql.createConnection(dbConfig);
+    console.log('connected');
+    const sql = 'SELECT * FROM `pets`';
+    const [rows] = await conn.query(sql);
+    res.json(rows);
+  } catch (error) {
+    console.log('error in get pets ===', error.message);
+    res.status(500).json('something went wrong');
+  } finally {
+    conn?.end();
+  }
+});
+
 module.exports = vetRoutes;
