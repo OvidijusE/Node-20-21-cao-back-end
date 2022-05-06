4035d1c98d533bed27d0aefe7929304ae3c2a614
rokas <rokasra@gmail.com>
2022-05-06 10:06:02 +0300

    no log on dbsetup

--- src/dbSetup.js
+++ src/dbSetup.js
@@ -5,11 +5,10 @@ const PORT = process.env.PORT || 5000;
 // db config
 const dbConfig = {
   host: process.env.HOST,
-  user: process.env.USER,
+  user: process.env.MYSQL_USER,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
 };
-console.log('dbConfig===', dbConfig);
 
 module.exports = {
   PORT,
